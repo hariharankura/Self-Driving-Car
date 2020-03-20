e131d83cc2da50aa5f4961efd8b66b5314ac6a4a
Mohit Ingale <mohitingale5@gmail.com>
2020-03-20 15:14:38 -0700

    Driver code fix

--- projects/DRIVER/l5_application/driver_logic/src/driver_logic.h
+++ projects/DRIVER/l5_application/driver_logic/src/driver_logic.h
@@ -3,4 +3,4 @@
 #include "project.h"
 
 void driver_logic__process_ultrasonic_sensors_data(dbc_SENSOR_USONARS_s sensor_data);
-dbc_DRIVER_STEER_s driver_logic__get_motor_command(void);
+dbc_DRIVER_STEER_SPEED_s driver_logic__get_motor_command(void);
--- projects/DRIVER/l5_application/driver_logic/test/test_driver_logic.c
+++ projects/DRIVER/l5_application/driver_logic/test/test_driver_logic.c
@@ -2,13 +2,17 @@
 
 #include "driver_logic.c"
 
-static const uint32_t exceed_threshold = 600;
+static const uint16_t obstacle_near_threshold = 9;
+static const uint16_t obstacle_far_threshold = 11;
 
 void setUp() {}
 void tearDown() {}
 
 void test_driver_logic__process_ultrasonic_sensors_data(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
   test_sensor_data.SENSOR_USONARS_back = 100;
   test_sensor_data.SENSOR_USONARS_front = 200;
   test_sensor_data.SENSOR_USONARS_left = 300;
@@ -18,105 +22,132 @@ void test_driver_logic__process_ultrasonic_sensors_data(void) {
 }
 
 void test_driver_logic__get_motor_command_test_move_forward_turn_right(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_left = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_left = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_SOFT_RIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_FORWARD;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_FORWARD_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_forward_turn_left(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_right = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_right = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_SOFT_LEFT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_FORWARD;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_FORWARD_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_forward(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_STRAIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_FORWARD;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_FORWARD_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_forward_2(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_right = exceed_threshold;
-  test_sensor_data.SENSOR_USONARS_left = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_right = obstacle_near_threshold;
+  test_sensor_data.SENSOR_USONARS_left = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_STRAIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_FORWARD;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_FORWARD_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_backwards_turn_left(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_right = exceed_threshold;
-  test_sensor_data.SENSOR_USONARS_front = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_right = obstacle_near_threshold;
+  test_sensor_data.SENSOR_USONARS_front = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_SOFT_RIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_REVERSE;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_REVERSE_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_backwards_turn_right(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_left = exceed_threshold;
-  test_sensor_data.SENSOR_USONARS_front = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_left = obstacle_near_threshold;
+  test_sensor_data.SENSOR_USONARS_front = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_SOFT_LEFT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_REVERSE;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_REVERSE_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_backwards(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_front = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_front = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_STRAIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_REVERSE;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_REVERSE_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_move_backwards_2(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_front = exceed_threshold;
-  test_sensor_data.SENSOR_USONARS_left = exceed_threshold;
-  test_sensor_data.SENSOR_USONARS_right = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_front = obstacle_near_threshold;
+  test_sensor_data.SENSOR_USONARS_left = obstacle_near_threshold;
+  test_sensor_data.SENSOR_USONARS_right = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_STRAIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_REVERSE;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_REVERSE_at_SPEED;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
 
 void test_driver_logic__get_motor_command_test_stop(void) {
-  dbc_SENSOR_USONARS_s test_sensor_data = {};
-  test_sensor_data.SENSOR_USONARS_front = exceed_threshold;
-  test_sensor_data.SENSOR_USONARS_back = exceed_threshold;
+  dbc_SENSOR_USONARS_s test_sensor_data = {.SENSOR_USONARS_left = obstacle_far_threshold,
+                                           .SENSOR_USONARS_right = obstacle_far_threshold,
+                                           .SENSOR_USONARS_front = obstacle_far_threshold,
+                                           .SENSOR_USONARS_back = obstacle_far_threshold};
+  test_sensor_data.SENSOR_USONARS_front = obstacle_near_threshold;
+  test_sensor_data.SENSOR_USONARS_back = obstacle_near_threshold;
   driver_logic__process_ultrasonic_sensors_data(test_sensor_data);
-  dbc_DRIVER_STEER_s motor_info = driver_logic__get_motor_command();
-  dbc_DRIVER_STEER_s verify_motor_info = {};
+  dbc_DRIVER_STEER_SPEED_s motor_info = driver_logic__get_motor_command();
+  dbc_DRIVER_STEER_SPEED_s verify_motor_info = {};
   verify_motor_info.DRIVER_STEER_direction = DRIVER_STEER_direction_STRAIGHT;
-  verify_motor_info.DRIVER_STEER_move = DRIVER_STEER_move_STOP;
-  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_s));
+  verify_motor_info.DRIVER_STEER_move_speed = DRIVER_STEER_move_STOP;
+  TEST_ASSERT_EQUAL_MEMORY(&motor_info, &verify_motor_info, sizeof(dbc_DRIVER_STEER_SPEED_s));
 }
