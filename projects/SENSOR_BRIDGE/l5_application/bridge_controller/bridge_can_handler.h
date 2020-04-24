5b8687b80bf63206edecfd2827755eda50cc73bd
Nicholas Kaiser <NicholasKaiser@nicholass-air.attlocal.net>
2020-04-23 17:48:41 -0700

    sensor module changes

--- projects/SENSOR_BRIDGE/l5_application/bridge_controller/bridge_can_handler.h
+++ projects/SENSOR_BRIDGE/l5_application/bridge_controller/bridge_can_handler.h
@@ -1,7 +1,6 @@
 #pragma once
 
 void bridge_can_handler__transmit_messages_10hz(void);
-void bridge_can_handler__transmit_start_stop_condition(void);
 
 // for testing purposes
 void bridge_can_handler__handle_all_incoming_messages(void);
