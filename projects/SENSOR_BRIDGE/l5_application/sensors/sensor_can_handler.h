a67a5b2a92e57a3fe6095e71963c91cdfe0e1a47
Nicholas Kaiser <NicholasKaiser@nicholass-air.attlocal.net>
2020-04-23 17:35:59 -0700

    updated code from dev/integrate

--- projects/SENSOR_BRIDGE/l5_application/sensors/sensor_can_handler.h
+++ projects/SENSOR_BRIDGE/l5_application/sensors/sensor_can_handler.h
@@ -2,9 +2,8 @@
 
 // for testing purposes
 void sensor_can_handler__transmit_messages_1hz(void);
-void sensor_can_handler__transmit_messages_10hz(void);
 
-void sensor_can_handler__transmit_messages_50hz(void);
+void sensor_can_handler__transmit_messages_10hz(void);
 
 // for testing purposes
 void sensor_can_handler__handle_all_incoming_messages(void);
--- projects/SENSOR_BRIDGE/l5_application/sensors/ultrasonic_sensor_handler.h
+++ projects/SENSOR_BRIDGE/l5_application/sensors/ultrasonic_sensor_handler.h
@@ -2,18 +2,7 @@
 
 #include <stdint.h>
 
-typedef struct {
-  uint16_t left;
-  uint16_t right;
-  uint16_t front;
-  uint16_t back;
-} sensor_t;
-
 void ultrasonic_sensor_handler__initialize_sensors(void);
-
-void ultrasonic_sensor_handler__get_all_sensor_values(sensor_t *sensor_values);
-
-void ultrasonic_sensor_handler__set_all_sensor_values(uint16_t left, uint16_t right, uint16_t front, uint16_t back);
 
 uint16_t ultrasonic_sensor_handler__get_sensor_value_left(void);
 
