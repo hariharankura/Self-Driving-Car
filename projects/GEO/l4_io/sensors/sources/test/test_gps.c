7b8ac62caa91bb76da03404785e9a14930030191
Shreya <shreyadinesh.patankar@sjsu.edu>
2020-04-26 23:41:06 -0700

    added for project progress part 2

--- projects/GEO/l4_io/sensors/sources/test/test_gps.c
+++ projects/GEO/l4_io/sensors/sources/test/test_gps.c
@@ -6,8 +6,8 @@
 #include "Mockgpio.h"
 #include "Mockuart.h"
 
+#include "Mockboard_io.h"
 #include "Mockqueue.h"
-
 // Use the real implementation (not mocks) for:
 #include "line_buffer.h"
 
@@ -24,25 +24,34 @@ void tearDown(void) {}
 
 void test_gps__init(void) { __gps_init(); }
 void test_gps_other_stuff(void) {}
-
+gpio_s test_gpio_op = {1, 18};
 void test_gps__parse_nema_string(void) {
+
   gps_coordinates_t temp_coordinates;
   char line_N_W[] = "$GPGGA,230612.015,667.0013,N,12102.4634,W,2,04,5.7,508.3,M,,,,0000*13";
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__reset_Expect(test_gpio_op);
   TEST_ASSERT_TRUE(gps__parse_nema_string(line_N_W, &temp_coordinates));
   TEST_ASSERT_EQUAL_FLOAT(7.116688, temp_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(-121.041057, temp_coordinates.longitude);
 
   char line_S_W[] = "$GPGGA,230612.015,667.0013,S,12102.4634,W,2,04,5.7,508.3,M,,,,0000*13";
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__reset_Expect(test_gpio_op);
   TEST_ASSERT_TRUE(gps__parse_nema_string(line_S_W, &temp_coordinates));
   TEST_ASSERT_EQUAL_FLOAT(-7.116688, temp_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(-121.041057, temp_coordinates.longitude);
 
   char line_N_E[] = "$GPGGA,230612.015,667.0013,N,12102.4634,E,2,04,5.7,508.3,M,,,,0000*13";
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__reset_Expect(test_gpio_op);
   TEST_ASSERT_TRUE(gps__parse_nema_string(line_N_E, &temp_coordinates));
   TEST_ASSERT_EQUAL_FLOAT(7.116688, temp_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(121.041057, temp_coordinates.longitude);
 
   char line_S_E[] = "$GPGGA,230612.015,667.0013,S,12102.4634,E,2,04,5.7,508.3,M,,,,0000*13";
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__reset_Expect(test_gpio_op);
   TEST_ASSERT_TRUE(gps__parse_nema_string(line_S_E, &temp_coordinates));
   TEST_ASSERT_EQUAL_FLOAT(-7.116688, temp_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(121.041057, temp_coordinates.longitude);
@@ -51,6 +60,8 @@ void test_gps__parse_nema_string(void) {
 void test_gps__parse_nema_string_temp(void) {
   gps_coordinates_t temp_coordinates;
   char line_N_W[] = "$GPGGA,230612.015,4.9310,S,0.8825,W,0,04,5.7,508.3,M,,,,0000*13";
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__reset_Expect(test_gpio_op);
   TEST_ASSERT_FALSE(gps__parse_nema_string(line_N_W, &temp_coordinates));
   TEST_ASSERT_EQUAL_FLOAT(-0.082183, temp_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(-0.01470833, temp_coordinates.longitude);
@@ -59,6 +70,8 @@ void test_gps__parse_nema_string_temp(void) {
 void test_gps__parse_nema_string_invalid_data(void) {
   gps_coordinates_t temp_coordinates;
   char line[] = "$GPGGA,230612.015,667.0013,N,12102.4634,W,0,04,5.7,508.3,M,,,,0000*13";
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__reset_Expect(test_gpio_op);
   TEST_ASSERT_FALSE(gps__parse_nema_string(line, &temp_coordinates));
   TEST_ASSERT_EQUAL_FLOAT(7.116688, temp_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(-121.041057, temp_coordinates.longitude);
@@ -101,6 +114,8 @@ void test_gps_handle_line(void) {
   for (int i = 0; i < sizeof(gps_string); i++) {
     line_buffer__add_byte(&line, gps_string[i]);
   }
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__set_Expect(test_gpio_op);
   gps__handle_line();
   TEST_ASSERT_EQUAL_FLOAT(48.133967, parsed_coordinates.latitude);
   TEST_ASSERT_EQUAL_FLOAT(11.533333, parsed_coordinates.longitude);
@@ -109,6 +124,8 @@ void test_gps_handle_line(void) {
 void test_gps__get_data(void) {
   char byte;
   uart__get_StubWithCallback(uart__get_stub);
+  board_io__get_led3_ExpectAndReturn(test_gpio_op);
+  gpio__set_Expect(test_gpio_op);
   gps__absorb_data();
   gps__handle_line();
   gps__handle_line();
