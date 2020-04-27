f622faf312ce14bc580722a364e9acee9c7c8bd6
Nicholas Kaiser <NicholasKaiser@nicholass-air.attlocal.net>
2020-04-27 13:00:51 -0700

    updated branch with sensor code changes

--- projects/SENSOR_BRIDGE/l5_application/shared_bridge_sensors/test/test_led_handler.c
+++ projects/SENSOR_BRIDGE/l5_application/shared_bridge_sensors/test/test_led_handler.c
@@ -2,6 +2,7 @@
 
 #include "Mockboard_io.h"
 #include "Mockgpio.h"
+#include "Mockultrasonic_sensor_handler.h"
 
 #include "led_handler.h"
 
@@ -20,31 +21,58 @@ void test_led_handler__turn_off_all_sjtwo_leds(void) {
 }
 
 void test_led_handler__diagnostic_test_object_detection_led3(void) {
+  sensor_t sensor_values;
   gpio_s led3;
 
+  sensor_values.left = 300;
+  sensor_values.right = 300;
+  sensor_values.front = 300;
+  sensor_values.back = 300;
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__set_Expect(led3);
-  led_handler__diagnostic_test_object_detection_led3(300, 300, 300, 300);
+  led_handler__diagnostic_test_object_detection_led3(sensor_values);
 
+  sensor_values.left = 30;
+  sensor_values.right = 30;
+  sensor_values.front = 30;
+  sensor_values.back = 30;
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__reset_Expect(led3);
-  led_handler__diagnostic_test_object_detection_led3(30, 30, 30, 30);
+  led_handler__diagnostic_test_object_detection_led3(sensor_values);
 
+  sensor_values.left = 49;
+  sensor_values.right = 300;
+  sensor_values.front = 300;
+  sensor_values.back = 300;
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__reset_Expect(led3);
-  led_handler__diagnostic_test_object_detection_led3(49, 300, 300, 300);
+  led_handler__diagnostic_test_object_detection_led3(sensor_values);
 
+  sensor_values.left = 300;
+  sensor_values.right = 300;
+  sensor_values.front = 60;
+  sensor_values.back = 300;
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__reset_Expect(led3);
-  led_handler__diagnostic_test_object_detection_led3(300, 300, 60, 300);
+  led_handler__diagnostic_test_object_detection_led3(sensor_values);
 
+  sensor_values.left = 300;
+  sensor_values.right = 61;
+  sensor_values.front = 300;
+  sensor_values.back = 300;
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__set_Expect(led3);
-  led_handler__diagnostic_test_object_detection_led3(300, 61, 300, 300);
+  led_handler__diagnostic_test_object_detection_led3(sensor_values);
 }
 
 void test_led_handler__diagnostic_test_object_detection_leds_for_each_sensor(void) {
+  sensor_t sensor_values;
   gpio_s led0, led1, led2, led3;
+
+  sensor_values.left = 300;
+  sensor_values.right = 300;
+  sensor_values.front = 300;
+  sensor_values.back = 300;
 
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__set_Expect(led3);
@@ -57,7 +85,12 @@ void test_led_handler__diagnostic_test_object_detection_leds_for_each_sensor(voi
 
   board_io__get_led0_ExpectAndReturn(led0);
   gpio__set_Expect(led0);
-  led_handler__diagnostic_test_object_detection_leds_for_each_sensor(300, 300, 300, 300);
+  led_handler__diagnostic_test_object_detection_leds_for_each_sensor(sensor_values);
+
+  sensor_values.left = 60;
+  sensor_values.right = 60;
+  sensor_values.front = 60;
+  sensor_values.back = 60;
 
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__reset_Expect(led3);
@@ -70,7 +103,12 @@ void test_led_handler__diagnostic_test_object_detection_leds_for_each_sensor(voi
 
   board_io__get_led0_ExpectAndReturn(led0);
   gpio__reset_Expect(led0);
-  led_handler__diagnostic_test_object_detection_leds_for_each_sensor(60, 60, 60, 60);
+  led_handler__diagnostic_test_object_detection_leds_for_each_sensor(sensor_values);
+
+  sensor_values.left = 150;
+  sensor_values.right = 117;
+  sensor_values.front = 110;
+  sensor_values.back = 120;
 
   board_io__get_led3_ExpectAndReturn(led3);
   gpio__set_Expect(led3);
@@ -83,5 +121,5 @@ void test_led_handler__diagnostic_test_object_detection_leds_for_each_sensor(voi
 
   board_io__get_led0_ExpectAndReturn(led0);
   gpio__set_Expect(led0);
-  led_handler__diagnostic_test_object_detection_leds_for_each_sensor(150, 117, 110, 120);
+  led_handler__diagnostic_test_object_detection_leds_for_each_sensor(sensor_values);
 }
